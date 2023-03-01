1de617f6fae9a5ed3d0745adab5833bb361b119c
FaNat1zM <kerrok.dir@ya.ru>
2023-03-01 23:21:45 +0300

    dev /tests:      тест объектов.

--- tests/entities_test/main.cpp
+++ tests/entities_test/main.cpp
@@ -1,7 +1,35 @@
 #include <cassert>
 
-#include "../../src/CWRDEntityID.h"
+#include "../../src/CWRDEntity.h"
 
+
+
+
+bool f_TestObject() {
+    auto point = CPoint( 0, 1 );
+    auto texture = NGE::CTexture( 'x' );
+    auto object
+        = NWRD::CObject( point, texture );
+    std::cout << "object: " << object
+        << std::endl;
+
+    assert( object.f_IsNull()
+        == false );
+
+    assert( point
+        == object.f_GetPoint() );
+
+    assert( texture
+        == object.f_GetTexture() );
+
+
+    auto object_empty = NWRD::CObject();
+    assert( object_empty.f_IsNull()
+        == true );
+
+
+    return true;
+}
 
 
 
@@ -19,5 +47,6 @@ bool f_TestID() {
 
 
 int main( void ) {
+    f_TestObject();
     f_TestID();
 }
