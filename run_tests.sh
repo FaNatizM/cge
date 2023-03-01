09e97dfd418f9c643361ece2558283ce8534605f
FaNat1zM <kerrok.dir@ya.ru>
2023-03-01 23:10:42 +0300

    dev+struc /tools:      запуск и сборка игры и тестов.

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+# Переход в каталог проекта
+g_scpt_path="$( dirname ${0} )"
+g_this_path="${PWD}"
+cd "${g_scpt_path}"
+
+
+g_tests=()
+g_tests+=( tests/ge_test )
+
+for g_test in ${g_tests[*]}; do
+   bash "${g_test}/build_and_run.sh"
+done
+
+
+cd "${g_this_path}"
