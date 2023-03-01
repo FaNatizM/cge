094f63a18de5ba2db19f369439c08efa0d85bbde
FaNat1zM <kerrok.dir@ya.ru>
2023-03-01 23:15:42 +0300

    dev src:      новые макросы для вариаций pimpl.

--- src/HMacroses.h
+++ src/HMacroses.h
@@ -4,6 +4,73 @@
 
 
 
+#define M_POINTER_TYPE( a_class, a_type )\
+    class a_class;\
+    using a_type\
+        = std::shared_ptr< a_class >;\
+
+
+
+#define M_IMPL_UNIQUE_DECL( a_class )\
+    public:\
+        ~a_class();\
+        a_class( a_class&& a_other );\
+        a_class& operator=(\
+            a_class&& a_other );\
+        a_class(\
+            const a_class& a_other );\
+        a_class& operator=(\
+            const a_class& a_other );\
+
+
+
+#define M_IMPL_UNIQUE_V_DECL( a_class )\
+    public:\
+        virtual ~a_class();\
+        a_class( a_class&& a_other );\
+        a_class& operator=(\
+            a_class&& a_other );\
+        a_class(\
+            const a_class& a_other );\
+        a_class& operator=(\
+            const a_class& a_other );\
+
+
+
+#define M_IMPL_SHARED_DECL( a_class )\
+    public:\
+        ~a_class() = default;\
+        a_class( a_class&& a_other )\
+            = default;\
+        a_class& operator=(\
+            a_class&& a_other )\
+            = default;\
+        a_class(\
+            const a_class& a_other )\
+            = default;\
+        a_class& operator=(\
+            const a_class& a_other )\
+            = default;\
+
+
+
+#define M_IMPL_SHARED_V_DECL( a_class )\
+    public:\
+        virtual ~a_class() = default;\
+        a_class( a_class&& a_other )\
+            = default;\
+        a_class& operator=(\
+            a_class&& a_other )\
+            = default;\
+        a_class(\
+            const a_class& a_other )\
+            = default;\
+        a_class& operator=(\
+            const a_class& a_other )\
+            = default;\
+
+
+
 #define M_IMPL_UNIQUE( a_class, a_impl )\
 \
 a_class::~a_class() = default;\
@@ -50,10 +117,21 @@ a_class& a_class::operator=(\
 
 
 
-#define M_IMPL_STRUCT( a_struct, a_type )\
+#define M_IMPL_SHARED_STRUCT(\
+    a_struct, a_type )\
     struct a_struct; \
     using a_type \
         = std::shared_ptr< \
+           a_struct >; \
+    a_type m_impl; \
+
+
+
+#define M_IMPL_UNIQUE_STRUCT(\
+    a_struct, a_type )\
+    struct a_struct; \
+    using a_type \
+        = std::unique_ptr< \
            a_struct >; \
     a_type m_impl; \
 
