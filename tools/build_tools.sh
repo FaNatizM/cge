a3ea9041029ac9fcbfcc0e39b25bf289aa5cc99b
FaNat1zM <kerrok.dir@ya.ru>
2023-03-01 21:38:25 +0300

    dev+struc /tools:      скрипт для сборки.

---
+++ tools/build_tools.sh
@@ -0,0 +1,187 @@
+# Библиотека средств сборки
+# Функция очистки консоли
+f_BT_Clear() {
+   clear
+   clear
+}
+
+
+
+# Получение списка исходников
+# из каталога
+f_BT_GetSrcList() {
+   local a_path="${1}"
+
+
+   [ ! -d "${a_path}" ] && return -1
+
+   local filter='grep -P ".h$|.cpp$|.hpp$"'
+   local src=( $( ls "${a_path}" | ${filter} ) )
+
+
+   local file=
+   local src_list=()
+   for file in ${src[*]}; do
+      src_list+=( "${a_path}/${file}" )
+   done
+
+   echo "${src_list[*]}"
+   return 0
+}
+
+
+
+# Функция создания списка исхрдников
+# из файла с относительными путями к ним
+f_BT_CreateSrcList() {
+   local a_project="${1}"
+   local a_list="${2}"
+
+
+   [ ! -d "${a_project}" ] && return -1
+   [ ! -f "${a_list}" ] && return -1
+
+   local src=( $( tr -d '\0' < "${a_list}" ) )
+
+
+   local file=
+   local src_list=()
+   for file in ${src[*]}; do
+      src_list+=( "${a_project}/${file}" )
+   done
+
+   echo "${src_list[*]}"
+   return 0
+}
+
+
+
+f_BT_CreateBuildDir() {
+   local a_path="${1}"
+
+   # Создание каталога сборочных файлов
+   if [ ! -d "${a_path}" ]; then
+      mkdir -p "${a_path}"
+   fi
+
+   if [ ! -d "${a_path}" ]; then
+      return -1
+   fi
+
+   return 0
+}
+
+
+
+f_BT_BuildExeByCLang() {
+   local a_build_path="${1}"
+   local a_src_path="${2}"
+   local a_src=( "$(echo ${3} )" )
+
+
+   local command="clang++ ${a_src[*]}"
+   echo -e "\n${command}\n"
+   ${command}
+   local result=${?}
+
+
+   # Чистка src
+   local objects=( $( ls "${a_src_path}" | grep -e '.gch' ) )
+   local file=
+   for file in ${objects[*]}; do
+      mv -v "${a_src_path}/${file}" "${a_build_path}"
+   done
+
+
+   return ${result}
+}
+
+
+
+# Перемещение и запуск исполняемого файла
+f_BT_MoveAndRun() {
+   local a_bin_from="${1}"
+   local a_bin="${2}"
+
+
+   if [ ! -f "${a_bin_from}" ]; then
+      return -1
+   fi
+
+
+   mv -v "${a_bin_from}" "${a_bin}"
+   if [ ! -f "${a_bin}" ]; then
+      return -1
+   fi
+
+   chmod +x "${a_bin}"
+
+
+   if [ -f "${a_bin}" ]; then
+      "${a_bin}"
+      return ${?}
+   fi
+
+   return -1
+}
+
+
+
+f_BT_BuildAndRunByCLang() {
+   local a_args=()
+   local args_count=0
+   while [ -n "${1}" ]; do
+      a_args+=( "${1}" )
+      args_count=$[${args_count}+1]
+      shift
+   done
+
+   local a_project_path=
+   local a_build_path=
+   local a_src_path=
+   local a_src_list=
+   local a_bin=
+
+   if [ ${args_count} -eq 4 ]; then
+      a_project_path=${a_args[0]}
+      a_build_path=${a_args[1]}
+      a_src_path=${a_args[2]}
+      a_bin=${a_args[3]}
+   fi
+
+   if [ ${args_count} -eq 5 ]; then
+      echo -e "\nBuild by src file"
+      a_project_path=${a_args[0]}
+      a_build_path=${a_args[1]}
+      a_src_path=${a_args[2]}
+      a_src_list=${a_args[3]}
+      a_bin=${a_args[4]}
+   fi
+
+
+   #f_BT_Clear
+
+
+   local src=()
+   if [ -f "${a_src_list}" ]; then
+      src=( "$( f_BT_CreateSrcList ${a_project_path} ${a_src_list} )" )
+   else
+      src=( "$( f_BT_GetSrcList ${a_src_path} )" )
+   fi
+
+   [ ${?} -ne 0 ] && return -1
+
+
+   f_BT_CreateBuildDir "${a_build_path}"
+   [ ${?} -ne 0 ] && return -2
+
+
+   f_BT_BuildExeByCLang "${a_build_path}" "${a_src_path}" "${src[*]}"
+   [ ${?} -ne 0 ] && return -3
+
+
+   f_BT_MoveAndRun "./a.out" "${a_bin}"
+   local result=${?}
+   [ ${result} -ne 0 ] && return -4
+   return ${result}
+}
