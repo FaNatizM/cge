5d7cc2d5a3740096dbff0e8fab71b49550765e33
sp <sp@sp.com>
2023-03-04 09:39:28 +0300

    dev /src+/test+/tools:      cmake using.

--- run_tests.sh
+++ run_tests.sh
@@ -5,14 +5,18 @@ cd "${g_scpt_path}"
 
 
 g_tests=()
-
-# g_tests+=( tests/ge_test )
-# g_tests+=( tests/wrd_test )
+g_tests+=( tests/ge_test )
+g_tests+=( tests/wrd_test )
 g_tests+=( tests/entities_test )
+
 
 for g_test in ${g_tests[*]}; do
    echo -e "\n\n${g_test}"
-   bash "${g_test}/build_and_run.sh"
+   rm -rf ./build
+   bash "${g_test}/cmake_and_run.sh"
+   if [ ${?} -ne 0 ]; then
+      echo "\nTest ${g_test} has been failed!"
+   fi
 done
 
 
