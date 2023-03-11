231eaaf6493cce2118cf291b510758c4380aeab6
FaNatizM <whitelight92@mail.ru>
2023-03-11 11:16:59 +0300

    dev+struc /src+/tests:______класс модели цепочки объектов.

---
+++ src/CWRDModelChain.cpp
@@ -0,0 +1,159 @@
+#include "CWRDModelChain.h"
+
+#include <cassert>
+#include <vector>
+
+
+
+
+namespace NWRD {
+    using TObjects
+        = std::vector< CObject >;
+
+
+
+
+    struct CModelChain::SImpl {
+        public:
+            explicit SImpl(
+                const CObject& a_head
+                , const CObject& a_body
+                , const CObject& a_tail
+                );
+
+            M_IMPL_MAKE_STRUCT(
+                SImpl, TImpl )
+
+
+        public:
+
+            // Объекты
+            TObjects m_objects;
+    };
+}
+
+
+
+
+NWRD::CModelChain::SImpl::SImpl(
+    const CObject& a_head
+    , const CObject& a_body
+    , const CObject& a_tail )
+    : m_objects() {
+
+    // Проверка смежности позиций
+    // объектов
+    m_objects.push_back( a_head );
+    m_objects.push_back( a_body );
+    m_objects.push_back( a_tail );
+}
+
+
+
+NWRD::CModelChain::CModelChain(
+    const CObject& a_head
+    , const CObject& a_body
+    , const CObject& a_tail )
+    : CModel()
+    , m_impl(
+        SImpl::f_Create(
+            a_head
+            , a_body
+            , a_tail ) ) {
+}
+
+
+
+NWRD::CObject
+NWRD::CModelChain::f_GetObject(
+    const int ) const {
+    return
+        m_impl->m_objects[ 0 ];
+}
+
+
+
+CPoint
+NWRD::CModelChain::f_GetPoint(
+    const int ) const {
+
+    return
+        m_impl->m_objects[ 0 ]
+            .f_GetPoint();
+}
+
+
+
+NGE::CTexture
+NWRD::CModelChain::f_GetTexture(
+    const int ) const {
+    return
+        m_impl->m_objects[ 0 ]
+            .f_GetTexture();
+}
+
+
+
+size_t
+NWRD::CModelChain::f_GetObjectsCount()
+    const {
+    return m_impl->m_objects.size();
+}
+
+
+
+void NWRD::CModelChain::f_Loop(
+    const TOperation& a_operation ) {
+
+    // a_operation( m_impl->m_object );
+}
+
+
+
+bool NWRD::CModelChain::f_Move(
+    const CPoint& a_point ) {
+    m_impl
+        ->m_objects[ 0 ]
+            .f_SetPoint( a_point );
+
+    return true;
+}
+
+
+
+std::ostream&
+NWRD::CModelChain::f_Visual(
+    std::ostream& a_out )
+    const {
+    a_out << "object: "
+        << m_impl->m_objects[ 0 ];
+    return a_out;
+}
+
+
+
+void NWRD::CModelChain::f_Test() {
+    /*
+    auto model = CModelChain();
+    std::cout << "model: "
+        << model << std::endl;
+
+    assert( model.f_GetObject()
+        .f_IsNull() == true );
+
+    assert( model.f_GetPoint()
+        .f_IsNull() == true );
+
+    assert( model.f_GetTexture()
+        .f_IsUndefined() == true );
+
+    assert( model.f_GetObjectsCount()
+        == 1 );
+
+
+    // Добавление объектов
+    assert(
+        model.f_Move( CPoint( 1, 1 ) )
+        == true );
+    */
+}
---
+++ src/CWRDModelChain.h
@@ -0,0 +1,77 @@
+#ifndef CWRDMODELCHAIN_H
+#define CWRDMODELCHAIN_H
+
+
+#include <iostream>
+
+#include "CWRDModel.h"
+
+
+
+
+namespace NWRD {
+    M_POINTER_TYPE(
+        CModelChain, TModelChain )
+
+
+
+    // Модель сущности, которая содержит
+    // цепочку объектов
+    class CModelChain : public CModel {
+        M_IMPL_SHARED_DECL( CModelChain )
+
+
+        public:
+            explicit CModelChain(
+                const CObject& a_head
+                , const CObject& a_body
+                , const CObject& a_tail
+            );
+
+        public:
+            M_MAKE_SHARED(
+                CModelChain
+                , TModelChain )
+
+            CObject
+            f_GetObject(
+                const int a_index = 0 )
+                const override;
+
+            CPoint f_GetPoint(
+                const int a_index = 0 )
+                const override;
+
+            NGE::CTexture
+            f_GetTexture(
+                const int a_index = 0 )
+                const override;
+
+            size_t f_GetObjectsCount()
+                const override;
+
+            void f_Loop(
+                const TOperation&
+                a_operation ) override;
+
+            bool f_Move(
+                const CPoint& a_point )
+                override;
+
+            std::ostream& f_Visual(
+                std::ostream& a_out )
+                const override;
+
+            static void f_Test();
+
+
+        private:
+            M_IMPL_SHARED_STRUCT(
+                SImpl, TImpl )
+    };
+}
+
+
+
+
+#endif
