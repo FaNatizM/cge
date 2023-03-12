b79ab9e440837eef84c738b6d0bd904189fffd64
sp <sp@sp.com>
2023-03-12 13:10:16 +0300

    dev /tests:      тест модели цепочки.

--- tests/snake_test/main.cpp
+++ tests/snake_test/main.cpp
@@ -4,149 +4,10 @@
 #include <iostream>
 
 #include "../../src/CWRDModelPoint.h"
+#include "../../src/CWRDModelChain.h"
 #include "../../src/CWRDViewer.h"
 #include "../../src/CWRDItemsCreator.h"
 
-
-
-
-bool f_TestPlace() {
-    auto nowhere
-        = NWRD::CPlace::f_MakeNowhere();
-
-    auto nowhere_1
-        = NWRD::CPlace::f_MakeNowhere();
-
-    std::cout << nowhere_1 << std::endl;
-
-
-    auto ground
-        = NWRD::CPlace::f_MakeGround();
-
-    std::cout << ground << std::endl;
-
-
-    return true;
-}
-
-
-
-bool f_TestLocation() {
-    auto location
-        = NWRD::CLocation( 3, 3 );
-    std::cout << location << std::endl;
-
-
-    auto nowhere
-        = NWRD::CPlace::f_MakeNowhere();
-    location.f_SetPlace(
-        CPoint( 1, 1 ), nowhere );
-    std::cout << location << std::endl;
-
-    assert( nowhere.f_IsEmpty()
-        == location.f_IsEmpty(
-            CPoint( 1, 1 ) ) );
-
-
-    assert( location.f_ExistPlace(
-        CPoint() ) == false );
-
-    assert( location.f_ExistPlace(
-        CPoint( 0, 0 ) ) == true );
-
-
-    return true;
-}
-
-
-
-bool f_TestViewerSmall() {
-    std::cout
-        << "\n\nf_TestViewerSmall()\n";
-    auto scene
-        = NGE::CScene( 3, 3 );
-    auto location
-        = NWRD::CLocation( 4, 4 );
-    auto viewer
-        = NWRD::CViewer(
-            scene
-            , location );
-
-    auto nowhere
-        = NWRD::CPlace::f_MakeNowhere();
-    location.f_SetPlace(
-        CPoint( 1, 2 ), nowhere );
-
-    viewer.f_View();
-    scene.f_Draw();
-
-    assert( viewer.f_SetPoint(
-        CPoint( 1, 1 ) )
-            == true );
-    viewer.f_View();
-    scene.f_Draw();
-
-
-    assert( viewer.f_SetPoint(
-        CPoint( -1, 1 ) )
-            == false );
-
-    assert( viewer.f_SetPoint(
-        CPoint( 10, 1 ) )
-            == false );
-
-    assert( viewer.f_MoveUp()
-        == true );
-
-    assert( viewer.f_MoveLeft()
-        == true );
-
-    assert( viewer.f_MoveRight()
-        == true );
-
-    assert( viewer.f_MoveDown()
-        == true );
-
-    assert( viewer.f_MoveDown()
-        == false );
-
-
-    return true;
-}
-
-
-
-bool f_TestViewerBig() {
-    std::cout
-        << "\n\nf_TestViewerBig()\n";
-
-
-    auto scene
-        = NGE::CScene( 4, 4 );
-    auto location
-        = NWRD::CLocation( 3, 4 );
-    auto viewer
-        = NWRD::CViewer(
-            scene
-            , location );
-
-    auto nowhere
-        = NWRD::CPlace::f_MakeNowhere();
-    location.f_SetPlace(
-        CPoint( 0, 0 ), nowhere );
-
-    viewer.f_View();
-    scene.f_Draw();
-
-    assert( viewer.f_SetPoint(
-        CPoint( 1, 1 ) )
-            == false );
-    viewer.f_View();
-    scene.f_Draw();
-
-
-    return true;
-}
 
 
 
@@ -164,49 +25,100 @@ bool f_TestItemsMoves() {
             scene
             , location );
 
-    auto object = NWRD::CObject(
-        CPoint(), NGE::CTexture( 'x' ) );
-    auto items_creator
+    const auto head = NWRD::CObject(
+        CPoint( 0, 0 ), NGE::CTexture( 'x' ) );
+    const auto body = NWRD::CObject(
+        CPoint( 1, 0 ), head.f_GetTexture() );
+    const auto tail = NWRD::CObject(
+        CPoint( 2, 0 ), head.f_GetTexture() );
+    const auto model
+        = NWRD::CModelChain::f_Create(
+            head, body, tail
+        );
+    auto snake_creator
         = NWRD::CItemsCreator(
             NWRD::TItemType::EUndefined
-            , NWRD::CModelPoint::f_Create( object )
+            , model
     );
 
-    auto item = items_creator.f_Create();
-    auto item_id
-        = location.f_AddItem( item );
+    auto snake = snake_creator.f_Create();
+    auto snake_id
+        = location.f_AddItem( snake );
 
-
-    auto point = CPoint( 0, 2 );
-    assert( location.f_MoveItem(
-        item_id
-        , point )
-            == true );
-
-    std::cout << "item: "
-        << item << std::endl;
-    std::cout << "item point: "
-        << item->f_GetPoint()
-        << std::endl;
-
-    assert(
-        ( item->f_GetPoint() == point )
-            == true );
-
-
-    point = CPoint( 0, 1 );
-    assert( location.f_MoveItem(
-        item_id
-        , point )
-            == true );
-
-    assert(
-        ( item->f_GetPoint() == point )
-            == true );
+    std::cout << "snake: "
+        << snake << std::endl;
 
 
     viewer.f_View();
     scene.f_Draw();
+
+
+    // Проверка смещений
+    auto f_Move
+        = [ = ](
+            NWRD::CLocation& a_location
+            , const CPoint& a_point ) {
+        assert( a_location.f_MoveItem(
+            snake_id
+            , a_point )
+                == true );
+
+        std::cout << "snake: "
+            << snake << std::endl;
+
+        assert(
+            ( snake->f_GetPoint() == a_point )
+                == true );
+
+
+        viewer.f_View();
+        scene.f_Draw();
+    };
+
+
+    f_Move( location, CPoint( 0, 2 ) );
+    assert(
+        ( snake->f_GetPoint( 1 ) == CPoint( 1, 2 ) )
+            == true );
+    assert(
+        ( snake->f_GetPoint( 2 ) == CPoint( 2, 2 ) )
+            == true );
+
+
+    f_Move( location, CPoint( 0, 1 ) );
+    assert(
+        ( snake->f_GetPoint( 1 ) == CPoint( 0, 2 ) )
+            == true );
+    assert(
+        ( snake->f_GetPoint( 2 ) == CPoint( 1, 2 ) )
+            == true );
+
+
+    f_Move( location, CPoint( 0, 0 ) );
+    assert(
+        ( snake->f_GetPoint( 1 ) == CPoint( 0, 1 ) )
+            == true );
+    assert(
+        ( snake->f_GetPoint( 2 ) == CPoint( 0, 2 ) )
+            == true );
+
+
+    f_Move( location, CPoint( 1, 0 ) );
+    assert(
+        ( snake->f_GetPoint( 1 ) == CPoint( 0, 0 ) )
+            == true );
+    assert(
+        ( snake->f_GetPoint( 2 ) == CPoint( 0, 1 ) )
+            == true );
+
+
+    f_Move( location, CPoint( 2, 1 ) );
+    assert(
+        ( snake->f_GetPoint( 1 ) == CPoint( 1, 1 ) )
+            == true );
+    assert(
+        ( snake->f_GetPoint( 2 ) == CPoint( 1, 2 ) )
+            == true );
 
 
     return true;
@@ -215,9 +127,5 @@ bool f_TestItemsMoves() {
 
 
 int main( void ) {
-    f_TestPlace();
-    f_TestLocation();
-    f_TestViewerSmall();
-    f_TestViewerBig();
     f_TestItemsMoves();
 }
