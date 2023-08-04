8b315ab551079be40a0f8f436aeea6cd9d67183b
FaNatizM <whitelight92@mail.ru>
2023-08-04 15:46:01 +0300

    fix /snake:______real game draft.

--- examples/snake/main.cpp
+++ examples/snake/main.cpp
@@ -14,16 +14,9 @@ using namespace NCGE;
 
 
 int main() {
-    const auto test_passed
-        = NSnake::f_Test();
-    assert( test_passed == true );
-
-
-    /*
     CGame game;
     CEngine test( game );
     test.f_Exec();
-    */
 
 
     return 0;
