d453981c1c1c4a92fc7db87a38025692e420f6fe
FaNatizM <whitelight92@mail.ru>
2023-08-13 20:43:12 +0300

    struc /test:______events test.

---
+++ tests/events_test/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <cassert>
+#include <iostream>
+
+#include <cge/eng/CCGEEventManagerItem.h>
+
+
+
+
+bool f_TestEvents() {
+}
+
+
+
+int main( int argc, char* argv[] ) {
+    std::cout << argv[ 0 ]
+        << std::endl << std::endl;
+
+    f_TestEvents();
+}
