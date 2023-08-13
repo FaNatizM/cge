02382fa236eee4f44e37042288005ee7ebd6616c
FaNatizM <whitelight92@mail.ru>
2023-08-13 22:45:12 +0300

    dev /test:______simple event manager test.

--- tests/events_test/main.cpp
+++ tests/events_test/main.cpp
@@ -6,7 +6,41 @@
 
 
 
+using namespace NCGE;
+
+
+
+
 bool f_TestEvents() {
+    auto events_manager
+        = CEventManagerItem();
+
+
+    auto counter = 0;
+    TEventEmitter emitter_1
+        = [ &counter ]() -> bool {
+        counter++;
+    };
+    events_manager.f_AddEventEmitter(
+        CEventItem::EType::E_Moved
+        , emitter_1 );
+
+
+    TEventEmitter emitter_2
+        = [ &counter ]() -> bool {
+        counter++;
+        counter++;
+    };
+    events_manager.f_AddEventEmitter(
+        CEventItem::EType::E_Moved
+        , emitter_2 );
+
+
+    events_manager.f_EmitEvent(
+        CEventItem::EType::E_Moved );
+
+
+    assert( counter == 3 );
 }
 
 
